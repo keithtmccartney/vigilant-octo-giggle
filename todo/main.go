ec69e9cf080bd599f7fa529019275da5d2169c6e
Keith <keithtmccartney@hotmail.com>
2018-11-23 22:32:29 +0000

    Implementing the 'createTodo' function.

--- todo/main.go
+++ todo/main.go
@@ -51,3 +51,14 @@ type (
         Completed bool `json:"completed"`
     }
 )
+
+// createTodo add a new todo
+func createTodo(c *gin.Context) {
+    completed, _ := strconv.Atoi(c.PostForm("completed"))
+
+    todo := todoModel{Title: c.PostForm("title"), Completed: completed}
+
+    db.Save(&todo)
+
+    c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID})
+}
